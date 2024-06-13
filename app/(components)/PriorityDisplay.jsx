12143a4eaf719071f2ef9038e067b52ab7de8c2b
Daniel Reid <dan.reid3005@gmail.com>
2024-06-13 12:30:07 +0100

    Ticket app completed

--- app/(components)/PriorityDisplay.jsx
+++ app/(components)/PriorityDisplay.jsx
@@ -6,23 +6,31 @@ const PriorityDisplay = ({ priority }) => {
     <div className="flex justify-start align-baseline">
       <FontAwesomeIcon
         icon={faFire}
-        className={`pr-0 ${priority > 0 ? "text-red-400" : "text-slate-400"}`}
+        className={` pr-1 ${
+          priority > 0 ? " text-red-400" : " text-slate-400"
+        }`}
       />
       <FontAwesomeIcon
         icon={faFire}
-        className={`pr-1 ${priority > 0 ? "text-red-400" : "text-slate-400"}`}
+        className={` pr-1 ${
+          priority > 1 ? " text-red-400" : " text-slate-400"
+        }`}
       />
       <FontAwesomeIcon
         icon={faFire}
-        className={`pr-2 ${priority > 0 ? "text-red-400" : "text-slate-400"}`}
+        className={`  pr-1 ${
+          priority > 2 ? " text-red-400" : " text-slate-400"
+        }`}
       />
       <FontAwesomeIcon
         icon={faFire}
-        className={`pr-3 ${priority > 0 ? "text-red-400" : "text-slate-400"}`}
+        className={` pr-1 ${
+          priority > 3 ? " text-red-400" : " text-slate-400"
+        }`}
       />
       <FontAwesomeIcon
         icon={faFire}
-        className={`pr-4 ${priority > 0 ? "text-red-400" : "text-slate-400"}`}
+        className={` ${priority > 4 ? " text-red-400" : " text-slate-400"}`}
       />
     </div>
   );
--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,9 +1,32 @@
-const StatusDisplay = () => {
-  return (
-    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200">
-        done
-    </span>
-  )
-}
+const StatusDisplay = ({ status }) => {
+  const getColour = (status) => {
+    let color = "bg-slate-700";
+    switch (status.toLowerCase()) {
+      case "complete":
+        color = "bg-green-200";
+        return color;
 
-export default StatusDisplay
+      case "started":
+        color = "bg-yellow-200";
+        return color;
+
+      case "not started":
+        color = "bg-red-200";
+        return color;
+    }
+
+    return color;
+  };
+
+  return (
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColour(
+        status
+      )}`}
+    >
+      {status}
+    </span>
+  );
+};
+
+export default StatusDisplay;
